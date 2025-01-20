cd132ecd3106f429d4e6c65af19c31553dea1a71
bandwith <62576+bandwith@users.noreply.github.com>
2025-01-20 10:19:40 -0700

    update dockerfile with args that allow zabbix image to be passed through at build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 tag=$1
 args=$2
-docker buildx build . --tag ghcr.io/fullmetal-fred/zabbix-proxy:$1 $2
+docker buildx build --build-arg ZABBIX_VERSION=$1 . --tag ghcr.io/fullmetal-fred/zabbix-proxy:$1 $2
